1660a23dff744b469c98e2ec09698bac0e3b3255
Rishiraj Pathak <rishirajraj124@gmail.com>
2025-06-19 09:11:26 +0530

    function practice ques added

--- Quick Revision/functions_JS/script.js
+++ Quick Revision/functions_JS/script.js
@@ -116,25 +116,155 @@ function_name();   // calling functions */
 
 // Create a function that checks if a number is even or odd.
 
-function evenOdd(num) {
-  if (num % 2 == 0) {
-    console.log("Even number");
-  } else {
-    console.log("Odd number");
-  }
-}
-evenOdd(4);
+// function evenOdd(num) {
+//   if (num % 2 == 0) {
+//     console.log("Even number");
+//   } else {
+//     console.log("Odd number");
+//   }
+// }
+// evenOdd(4);
+
+// Write an arrow function that returns the square of a number.
+
+// const square = (num) => {
+//   return num ** 2;
+// }
+// console.log(`Square =  ${square(5)}`);
+
+// Write a function expression that returns the factorial of a number.
+
+// function fact(num) {
+//   let factorial = 1
+//   if (num === 0 || num === 1) {
+//     return 1;
+//   } else {
+//      return num * fact(num - 1);
+//   }
+// }
+// console.log(fact(5));
+
+// Write a function that says hello. If no name is given, it should default to "Guest".
+
+// function sayHello(name = "Guest"){
+//   return `Hello ${name}!`;
+// }
+
+// console.log(sayHello());
+// console.log(sayHello("John Doe"));
+
+// // Convert this function declaration to a function expression:
+// function add(a, b) {
+//   return a + b;
+// }
+
+// // function expression
+// const add = (a,b) => {
+//    return a + b;
+// };
+// console.log(`Addition of two numbers = ${add(5,10)}`);
+
+// Write a function `calculate` that takes two numbers and a callback (add, subtract, etc.)
+// Example: calculate(5, 3, subtract) → 2
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function subtract(a, b) {
+//   return a - b;
+// }
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// function divide(a, b) {
+//   return a / b;
+// }
+
+// const calculate = (a, b, operation) => {
+//   return operation(a, b);
+// };
+
+// console.log(`Addition = ${calculate(5, 5, add)}`);
+// console.log(`Subtraction = ${calculate(5, 5, subtract)}`);
+// console.log(`Multiplication = ${calculate(5, 5, multiply)}`);
+// console.log(`Division = ${calculate(5, 5, divide)}`);
+
+// Create a function `multiplier` that returns a function.
+// multiplier(3)(5) should return 15
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// const multiplier = (a, b, operation) => {
+//   return operation(a, b);
+// };
+
+// console.log(`Multiplication = ${multiplier(3, 5, multiply)}`);
+
+// Write a function max(a, b) that returns the larger number.
+
+// function max(a,b){
+//   if(a > b){
+//     console.log("a is greater");
+//   }
+//   else{
+//     console.log("b is greater");
+//   }
+// }
+// max(8,5)
+
+// Write a function calculator(a, b, op) where op is one of "+", "-", "*", "/".
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// function mul(a, b) {
+//   return a * b;
+// }
+
+// function div(a, b) {
+//   return a / b;
+// }
+
+// const calculator = (a, b, operation) => {
+//   return operation(a, b);
+// };
+
+// console.log(`Addition = ${calculator(10, 5, add)}`);
+// console.log(`Subtraction = ${calculator(10, 5, sub)}`);
+// console.log(`Multiplication = ${calculator(10, 5, mul)}`);
+// console.log(`Division = ${calculator(10, 5, div)}`);
+
+// Convert this to a function expression:
+/* function sayHi() {
+  console.log("Hi!");
+} */
+
+// const sayHi = () => {
+//   console.log("Hi!");
+// };
+// sayHi();
+
 
 // High Order and Callback functions.
 // Hight order function takes the another function as a parameter/agrument inside the another function.
 // Callbacks are the functions which are used to use the parameter as a function.
 
 //Eg.
-function add(a, b, cb) {
-  let result = a + b;
-  cb(result);
-}
+// function add(a, b, cb) {
+//   let result = a + b;
+//   cb(result);
+// }
 
-add(4, 5, function (val) {
-  console.log(val);
-});
+// add(4, 5, function (val) {
+//   console.log(val);
+// });
